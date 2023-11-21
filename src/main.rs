aafc4c14b7821c71ffcb9d1c4b22353e9e100bf0
eugenioclrc <eugenioclrc@gmail.com>
2023-11-21 07:00:38 -0300

    add test

--- src/main.rs
+++ src/main.rs
@@ -5,23 +5,38 @@ use pest::Parser;
 #[grammar = "Huff.pest"] // This should point to your huff.pest file
 pub struct HuffParser;
 
+// return parsed file or error
+fn parse(input: &str) -> Result<pest::iterators::Pairs<Rule>, pest::error::Error<Rule>> {
+    let pairs = HuffParser::parse(Rule::huff, input)?;
+    Ok(pairs)
+}
+
 fn main() {
     let unparsed_file = "// comment :D\n"; // Replace this with actual Huff code
-
-    match HuffParser::parse(Rule::huff, &unparsed_file) {
-        Ok(pairs) => {
-            for pair in pairs {
-                // Process the parsed pairs
-                println!("Rule: {:?}, Span: {:?}", pair.as_rule(), pair.as_span());
-            }
-        }
-        Err(error) => {
-            println!("Error parsing file: {}", error);
-        }
-    }
+    let parsed_file = parse(unparsed_file).unwrap();
+    for line in parsed_file {
+        println!("Rule:    {:?}", line.as_rule());
+        println!("Span:    {:?}", line.as_span());
+        println!("Text:    {}", line.as_str());
+        println!("Pairs:   {:?}", line.into_inner());
+    }       
+    
 }
 
 
 /*fn main() {
     println!("Hello, world!");
 }*/
+
+#[cfg(test)]
+mod tests {
+    use crate::parse;
+    #[test]
+
+    fn it_works_with_comments() {
+        let unparsed_file = "// comment :D\n"; // Replace this with actual Huff code
+
+        let results = parse(unparsed_file).unwrap();
+        assert_eq!(results.as_str(), unparsed_file);
+    }
+}
